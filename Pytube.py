bbe205d90acc5899871e7f839a86f9d8205d8e7a
LeviS <levisporto14@gmail.com>
2026-01-05 21:16:59 -0300

    pytube for downloading videos

---
+++ Pytube.py
@@ -0,0 +1,11 @@
+#remember to pip install pytubefix 
+from pytubefix import YouTube
+
+def Download(link):
+    youtube_video = YouTube(link)
+    youtube_video = youtube_video.streams.get_highest_resolution();
+    youtube_video.download()
+    print("Success!")
+
+link = input("Enter the link to the video:")
+Download(link)
