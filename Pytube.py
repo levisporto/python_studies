8a82f5b63c28627457d34dee52d95927c1339581
Linus Torvalds <torvalds@linux-foundation.org>
2026-01-15 21:25:21 -0300

    HMMM

---
+++ Pytube.py
@@ -0,0 +1,14 @@
+#remember to pip install pytubefix 
+from pytubefix import YouTube
+
+def Download(link):
+    youtube_video = YouTube(link)
+    youtube_video = youtube_video.streams.get_highest_resolution();
+    youtube_video.download()
+    print("Success!")
+
+link = input("Enter the link to the video:")
+Download(link)
+
+"lmao now LINUS TORVALDS is a contributor"
+
